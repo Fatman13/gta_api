039884ab1b4eb39539e6b080374512e66f37faea
fatman13 <tctctcly@gmail.com>
2017-03-24 16:38:06 +0800

    api 0.0.1 good to go

--- app.py
+++ app.py
@@ -8,27 +8,134 @@ import pprint
 import json
 import datetime
 
+import re
+from sqlalchemy.dialects.sqlite import DATE
+from sqlalchemy import cast
+
+d = DATE(
+		storage_format="%(year)04d/%(month)02d/%(day)02d",
+		regexp=re.compile("(?P<year>\d+)/(?P<month>\d+)/(?P<day>\d+)")
+	)
+
 def row2dict(tbl, row):
 	d = {}
 	for column in tbl.columns:
 		d[column.name] = str(getattr(row, column.name))
 	return d
 
-engine = create_engine('sqlite:///destServ.db', convert_unicode=True)
-metadata = MetaData(bind=engine)
-services = Table('destinationServices', metadata, autoload=True)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///destServ.db'
+db = SQLAlchemy(app)
 
-@app.route('/', methods=['GET'])
+class DestinationService(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	country = db.Column(db.String())
+	country_code = db.Column(db.String())
+	city = db.Column(db.String())
+	city_code = db.Column(db.String())
+	currency = db.Column(db.String())
+	item_name = db.Column(db.String())
+	item_code = db.Column(db.String())
+	duration = db.Column(db.String())
+	language = db.Column(db.String())
+	# dates_from = db.Column(db.String())
+	# dates_from = db.Column(db.DateTime)
+	dates_from = db.Column(d)
+	# dates_to = db.Column(db.String())
+	# dates_to = db.Column(db.DateTime)
+	dates_to = db.Column(d)
+	days = db.Column(db.String())
+	min = db.Column(db.String())
+	conditions = db.Column(db.String())
+	pax_type = db.Column(db.String())
+	price = db.Column(db.Float)
+	age_from = db.Column(db.Integer())
+	age_to = db.Column(db.Integer())
+	commision = db.Column(db.String())
+	languageCode_list = db.Column(db.String())
+	specialItem_code = db.Column(db.String())
+	net_price = db.Column(db.Float)
+	closed_date = db.Column(db.String())
+	commentary = db.Column(db.String())
+	meeting_point = db.Column(db.String())
+
+	def __init__(self, id, country, country_code, city, city_code, currency, item_name, item_code, duration, language, \
+					 dates_from, dates_to, days, min, conditions, pax_type, price, age_from, age_to, \
+					 commision, languageCode_list, specialItem_code, net_price, closed_date, commentary, meeting_point):
+		self.id = id
+		self.country = country
+		self.country_code = country_code
+		self.city = city
+		self.city_code = city_code
+		self.currency = currency
+		self.item_name = item_name
+		self.item_code = item_code
+		self.duration = duration
+		self.language = language
+		self.dates_from = dates_from
+		self.dates_to = dates_to
+		self.days = days
+		self.min = min
+		self.conditions = conditions
+		self.pax_type = pax_type
+		self.price = price
+		self.age_from = age_from
+		self.age_to = age_to
+		self.commision = commision
+		self.languageCode_list = languageCode_list
+		self.specialItem_code = specialItem_code
+		self.net_price = net_price
+		self.closed_date = closed_date
+		self.commentary = commentary
+		self.meeting_point = meeting_point
+
+	def __repr__(self):
+		# return "<DS %r %r %r>" % self.id, 'c: ' + self.city_code + '_' + self.item_code, 'n: ' + self.item_name
+		return "<DS id: {0} code: {1} name: {2}>".format(self.id, self.city_code + '_' + self.item_code, self.item_name)
+
+@app.route('/destinationServices', methods=['GET'])
 def ds():
-	# service = query_db('select * from destinationServices where country_code="CA"')
-	res = services.select(services.c.id == 1).execute().first()
-	pprint.pprint(res)
-	# return res
-	return json.dumps(row2dict(services, res), ensure_ascii=False)
-	# return row2dict(services, res)
-	# return json.dumps(res, cls=AlchemyEncoder)
-	# return json.dumps([dict(r) for r in res])
-	# return {'data': [dict(zip(tuple (res.keys()) ,i)) for i in res.cursor]}
+	city_code, item_code, from_d, to_d = request.args.get('cityCode'), request.args.get('itemCode'), request.args.get('fromDate'), request.args.get('toDate') 
+	# pprint.pprint('params: ' + str(city_code) + ' a ' + str(item_code) + ' a ' + str(from_d) + ' a ' + str(to_d) )
+	# res = services.select(services.c.id == 1).execute().first()
+	# pprint.pprint(res)
+
+	q = db.session.query(DestinationService)
+	if city_code != None:
+		q = q.filter(DestinationService.__table__.c.city_code == city_code)
+	if item_code != None:
+		q = q.filter(DestinationService.__table__.c.item_code == item_code)
+
+	data = []
+	from_date = datetime.datetime.strptime(from_d, "%Y-%m-%d").date() if from_d != None else None
+	to_date = datetime.datetime.strptime(to_d, "%Y-%m-%d").date() if to_d != None else None
+	# pprint.pprint('Dates: ' + str(from_date) + ' ' + str(to_date))
+	for row in q.all():
+		if from_date != None and to_date != None:
+			# pprint.pprint('Row Dates: ' + str(row.dates_from) + ' ' + str(row.dates_to))
+			if row.dates_from >= from_date and row.dates_to <= to_date:
+				# pprint.pprint("True?")
+				data.append(row2dict(DestinationService.__table__, row))
+			continue
+
+
+		data.append(row2dict(DestinationService.__table__, row))
+	res = {}
+	res['Data'] = data
+
+	return json.dumps(res, ensure_ascii=False)
+
+
+# engine = create_engine('sqlite:///destServ.db', convert_unicode=True)
+# metadata = MetaData(bind=engine)
+# services = Table('destinationServices', metadata, autoload=True)
+
+# @app.route('/destinationServices', methods=['GET'])
+# def ds():
+# 	city_code, item_code, from_d, to_d = request.args.get('cityCode'), request.args.get('itemcode'), request.args.get('fromdate'), request.args.get('todate') 
+# 	pprint.pprint('params: ' + str(city_code) + ' a ' + str(item_code) + ' a ' + str(from_d) + ' a ' + str(to_d) )
+# 	res = services.select(services.c.id == 1).execute().first()
+# 	# pprint.pprint(res)
+# 	return json.dumps(row2dict(services, res), ensure_ascii=False)
 
 if __name__ == "__main__":
 	app.run()
